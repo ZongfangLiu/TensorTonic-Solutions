d14c91a45a4ee2ab930b48c8294fc30ff4081db0
Zongfang Liu <130076473+ZongfangLiu@users.noreply.github.com>
2026-04-20 16:19:59 +0800

    Update solution for bag-of-words

--- bag-of-words/bag-of-words.py
+++ bag-of-words/bag-of-words.py
@@ -5,8 +5,4 @@ def bag_of_words_vector(tokens, vocab):
     Returns: np.ndarray of shape (len(vocab),), dtype=int
     """
     # Your code here
-    output = np.zeros(len(vocab), dtype=int)
-    for (i, word) in enumerate(vocab):
-        output[i] = tokens.count(word)
-
-    return output
+    return np.array([tokens.count(word) for word in vocab], dtype=int)
